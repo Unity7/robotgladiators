b297163e23571acd843c4e42d08af1b31be8a3b8
Bay <baybui112@gmail.com>
2021-02-20 19:55:37 -0800

    added ability to skip fights

--- game.js
+++ game.js
@@ -2,6 +2,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -17,6 +18,13 @@ var enemyAttack = 12;
 var fight = function() {
   window.alert("Welcome to Robot Gladiators!");
 
+  //Let Player Decide to Fight or SKIP
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+  // if player choses to fight, then fight
+  if (promptFight === "fight" || promptFight === "FIGHT") {
+
+
   // Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
 
 enemyHealth = enemyHealth - playerAttack;
@@ -24,6 +32,7 @@ enemyHealth = enemyHealth - playerAttack;
   // Log a resulting message to the console so we know that it worked.
 
 console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.")
+
 
 // check enemy's health
 if (enemyHealth <= 0) {
@@ -49,7 +58,26 @@ console.log( enemyName + " attacked " + playerName + ". " + playerName + " now h
   else {
     window.alert(playerName + " still has " + playerHealth + " health left.");
   }
+} 
+  // if player choses to skip
+  else if (promptFight === "skip" || promptFight === "SKIP") {
 
+  // confirm player wants to skip
+  var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+  // if yes (true), leave fight
+  if (confirmSkip) {
+    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+    // subtract money from playerMoney for skipping
+    playerMoney = playerMoney - 2;
+  }
+  // if no (false), ask question again by running fight() again
+  else {
+    fight();
+  }
+  } else {
+    window.alert("You need to choose a valid option. Try again!");
+  }
 };
 
 
